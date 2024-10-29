35b7457e35814c9943462cbd29e753c1788ad2b9
junaidasif-dev <junaidasifdev@gmail.com>
2024-10-29 17:42:38 +0500

    Added

--- Sem-5/DM/Lab/sd.py
+++
@@ -1,9 +0,0 @@
-# Read a csv file 
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-df = pd.read_csv('sales_data.csv')
-#print(df.head())
-df['amount'].plot(kind='box')
-plt.show()
